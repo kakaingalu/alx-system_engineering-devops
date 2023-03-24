3386c7a0b57bf67781ad430d332ad3bfe0b9bc1b
Wanton Raven <josephngalu96@gmail.com>
2023-03-24 15:07:57 -0700

    updated 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # set up your client SSH configuration file so that so as to connect to a server without typing a password.
 
-file_line {'Turn off passwd auth':
-  ensure => 'present'
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
   path   => '/etc/ssh/sshd_config',
   line   => 'PasswordAuthentication no',
 }
 
-file_line {'Declare identify file':
-  ensure => 'present'
+file_line { 'Declare identify file':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentifyFile ~/.ssh/school',
 }
