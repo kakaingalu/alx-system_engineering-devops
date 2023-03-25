f435692a81276fc60ba155b4eea435fadd10d8f1
Joseph Kakai <josephngalu96@gmail.com>
2023-03-25 05:13:00 -0700

    updated linter error on 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,13 @@
 # Define a file resource for /tmp/school
 file { '/tmp/school':
+  # Ensure that the file exists and has the specified properties
   ensure  => 'file',
+  # Set the file permission to 0744
   mode    => '0744',
+  # Set the file owner to www-data
   owner   => 'www-data',
+  # Set the file permission to 0744
   group   => 'www-data',
+  # Set the file contents to "I love puppet"
   content => 'I love Puppet',
 }
