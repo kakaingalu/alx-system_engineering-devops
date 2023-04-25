9ec1c4e81bf670aad0ced76c7399fff25d9df8c4
Joseph Kakai <josephngalu96@gmail.com>
2023-04-25 02:52:43 -0700

    handled the number of tasks issue

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,51 +6,30 @@ import csv
 import requests
 
 
-def employee_todo_progress():
-    # Make an HTTP GET request to retrieve the employee's
-    # to-do list from the REST API
-    response = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1])
-    )
-
-    # If the response status code is not 200 (OK), print an
-    # error message and return
-    if response.status_code != 200:
-        print('Error: {}'.format(response.status_code))
-        return
-
-    # Extract the to-do list items from the response JSON
-    # and the user ID from the first item
-    todos = response.json()
-    user_id = todos[0]['userId']
-
-    # Construct the file name for the CSV file using the user ID
-    file_name = '{}.csv'.format(user_id)
-
-    # Open the CSV file for writing and create a CSV writer object
-    # with the appropriate settings
-    with open(file_name, mode='w', newline='') as file:
-        writer = csv.writer(file,
-                            quoting=csv.QUOTE_ALL
-                            )
-
-        # Write a header row to the CSV file with column names
-        writer.writerow([
-            'USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'
-        ])
-
-        # Iterate over the to-do list items and write each one as a row
-        # in the CSV file
-        for todo in todos:
-            writer.writerow([
-                todo['userId'], 'username', todo['completed'], todo['title']
-            ])
-
-    # Print a success message with the name of the CSV file that was created
-    print('CSV file {} has been created successfully.'.format(file_name))
-
-
-# Check if this module is being run as the main program,
-# and if so, call the employee_todo_progress function
 if __name__ == '__main__':
-    employee_todo_progress()
+    # Retrieve the user ID from the command line argument
+    user_id = argv[1]
+    # Construct the URL to retrieve the user's information
+    user_info_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+
+    # Make a request to the API and extract the user's username
+    user_response = requests.get(user_info_url)
+    user_username = user_response.json().get('username')
+
+    # Construct the URL to retrieve the user's tasks
+    user_tasks_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
+
+    # Make a request to the API and convert the response to a JSON object
+    user_tasks = requests.get(user_tasks_url).json()
+
+    # Create a new CSV file and write the user's tasks to it
+    with open('{}.csv'.format(user_id), 'w', encoding='UTF8', newline='') as csv_file:
+        # Create a CSV writer object with quoting set to QUOTE_ALL
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+        # Loop through the tasks and write each one to a row in the CSV file
+        for task in user_tasks:
+            task_completed = task.get('completed')
+            task_title = task.get('title')
+            csv_writer.writerow([user_id, user_username, task_completed, task_title])
+
