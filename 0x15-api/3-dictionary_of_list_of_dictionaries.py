9345c722101c2bd2c3947c90c4e21a8e4158d7fa
Joseph Kakai <josephngalu96@gmail.com>
2023-04-26 06:58:37 -0700

    updated  3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,48 +1,50 @@
 #!/usr/bin/python3
 """This module returns information about employee's TODO list progress"""
 
-import requests
 import json
+import requests
 
 
-def all_employees_todo_progress():
-    response = requests.get('https://jsonplaceholder.typicode.com/todos')
-    if response.status_code != 200:
-        print(f"Error: {response.status_code}")
-        return
-
-    todos = response.json()
-    tasks = {}
-    for todo in todos:
-        user_id = todo['userId']
-        if user_id not in tasks:
-            tasks[user_id] = []
-        tasks[user_id].append({
-            "task": todo['title'],
-            "completed": todo['completed'],
-            "username": ""
-        })
-
-    # Now, for each user ID, we need to get the username
-    for user_id in tasks:
-        response = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                                f'{user_id}')
-
-        if response.status_code == 200:
-            username = response.json()['username']
-            for task in tasks[user_id]:
-                task['username'] = username
-        else:
-            print(f"Error: could not retrieve username for user ID {user_id}")
-
-    # Export the tasks to a JSON file
-    file_name = 'todo_all_employees.json'
-    with open(file_name, mode='w') as file:
-        json.dump(tasks, file)
-
-    print(f"JSON file {file_name} has been created successfully.")
-    return
-
-
+# check if this is main program
 if __name__ == "__main__":
-    all_employees_todo_progress()
+
+    # get information from api
+    url = "https://jsonplaceholder.typicode.com/users/"
+    users_dict = requests.get(url).json()
+
+    # filename defination
+    filename = "todo_all_employees.json"
+
+    # dictionary defination
+    dictionary = {}
+
+    # loop through users_dict
+    for data in users_dict:
+        # get infomation on user's name
+        user_name = data.get("username")
+
+        # get information on user's id
+        users_id = str(data.get("id"))
+
+        # get information on todo list for the user
+        request = requests.get(f"{url}{users_id}/todos").json()
+
+        # assigning the key user_id in dictionary with empty list
+        dictionary[users_id] = []
+
+        # loop through request
+        for item in request:
+            # declare empty dictionary
+            each_item = {}
+
+            # assign each key with corresponding value
+            each_item["username"] = user_name
+            each_item["task"] = item.get("title")
+            each_item["completed"] = item.get("completed")
+
+            # append all keys and the values into the key users_id
+            dictionary[users_id].append(each_item)
+
+    # write the json file
+    with open(filename, "w") as j_file:
+        json.dump(dictionary, j_file)
