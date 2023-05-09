c8540ba07d3972a24e20f4b50415a97e0b4484e0
Joseph Kakai <josephngalu96@gmail.com>
2023-05-09 08:41:26 -0700

    updated

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,25 +3,32 @@
 a module that queries the Reddit API and returns a list
 containing the titles of all hot articles
 """
-from requests import get
-after = None 
+import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=None, after=None):
     """
     a function that that queries the Reddit API and returns
     a list containing the titles of all hot article
     """
-    global after
-    head = {"User-Agent": "Version 1.51.110 Chromium: 113.0.5672.77"}
-    Url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    afta = {'limit': 100 'after': after}
-    response = get(Url, headers=head, params=afta, allow_redirects=False)
+    if hot_list is None:
+        hot_list = []
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 200:
-        posts = response.json()
-        after = posts.get("data").get("after")
-        (After := after) and recurse(subreddit, hot_list)
-        children = posts.get("data").get("children")
-        return [title.get("data").get("title") for title in children]
-    else:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            hot_list.append(post['data']['title'])
+        after = data['data']['after']
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return hot_list
+    elif response.status_code == 404:
         return None
+    else:
+        raise ValueError(f"Request failed with status code\
+                         {response.status_code}")
