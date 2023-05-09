8dd539a7cba4e9f729411c6e962cb44e06603c76
Joseph Kakai <josephngalu96@gmail.com>
2023-05-09 08:26:29 -0700

    updated pep8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""a module that queries the Reddit API and returns a list
-   containing the titles of all hot articles
 """
-
+a module that queries the Reddit API and returns a list
+containing the titles of all hot articles
+"""
 from requests import get
-after = None
+after = None 
 
 
 def recurse(subreddit, hot_list=[]):
-    """a function that that queries the Reddit API and returns
+    """
+    a function that that queries the Reddit API and returns
     a list containing the titles of all hot article
     """
     global after
