572dd295a33b8899e3af42be4177769c821c0c38
josephngalu96@gmail.com <yjosephngalu96@gmailcom>
2023-05-17 19:52:08 +0000

    created 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# increase amount of traffic on nginx
+
+# edit the nginx file in etc/default directory
+exec { 'edit-nginx-file':
+  command => '/bin/sed -i "s/15/10000/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+# reload nginx 
+exec { 'reload-nginx':
+  command => '/usr/sbin/service nginx restart',
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+}
