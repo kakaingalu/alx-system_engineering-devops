39740bb012ed6c8c423e5cda93e03f1feb79a3d4
josephngalu96@gmail.com <yjosephngalu96@gmailcom>
2023-05-18 11:12:10 +0000

    pulled files from github

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 # edit the nginx file in etc/default directory
 exec { 'edit-nginx-file':
-  command => '/bin/sed -i "s/15/10000/" /etc/default/nginx',
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
 }
 
